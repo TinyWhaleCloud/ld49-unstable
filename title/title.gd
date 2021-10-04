8197b6720547e114aa2029b36999f60a95b9fb96
binaryDiv <binarydiv@gmail.com>
2021-10-04 22:25:32 +0200

    Optimize title screen text and window centering

--- title/title.gd
+++ title/title.gd
@@ -3,15 +3,15 @@ class_name Title
 
 var settings
 
+onready var settings_container = $CanvasLayer/CenterContainer/ColorRect/SettingsContainer
 
 func _ready():
-    $Popup.popup_centered()
     settings = get_node("/root/Settings")
-    $Popup/ColorRect/SettingsContainer/CameraRotation.pressed = settings.camera_rotation
-    $Popup/ColorRect/SettingsContainer/ParallaxScrolling.pressed = settings.parallax_scrolling
-    $Popup/ColorRect/SettingsContainer/Sfx.pressed = settings.sfx
-    $Popup/ColorRect/SettingsContainer/Music.pressed = settings.music
-    $Popup/ColorRect/SettingsContainer/SkipTutorial.pressed = settings.skip_tutorial
+    settings_container.get_node("CameraRotation").pressed = settings.camera_rotation
+    settings_container.get_node("ParallaxScrolling").pressed = settings.parallax_scrolling
+    settings_container.get_node("Sfx").pressed = settings.sfx
+    settings_container.get_node("Music").pressed = settings.music
+    settings_container.get_node("SkipTutorial").pressed = settings.skip_tutorial
 
 
 
@@ -25,25 +25,21 @@ func _on_StartGameButton_pressed():
     get_tree().change_scene("res://main_game/main_game.tscn")
 
 
-func _on_Popup_hide():
-    $Popup.popup_centered()
-
-
 func _on_CameraRotation_pressed():
-    settings.camera_rotation = $Popup/ColorRect/SettingsContainer/CameraRotation.pressed
+    settings.camera_rotation = settings_container.get_node("CameraRotation").pressed
 
 
 func _on_ParallaxScrolling_pressed():
-    settings.parallax_scrolling = $Popup/ColorRect/SettingsContainer/ParallaxScrolling.pressed
+    settings.parallax_scrolling = settings_container.get_node("ParallaxScrolling").pressed
 
 
 func _on_Sfx_pressed():
-    settings.sfx = $Popup/ColorRect/SettingsContainer/Sfx.pressed
+    settings.sfx = settings_container.get_node("Sfx").pressed
 
 
 func _on_Music_pressed():
-    settings.music = $Popup/ColorRect/SettingsContainer/Music.pressed
+    settings.music = settings_container.get_node("Music").pressed
 
 
 func _on_SkipTutorial_pressed():
-    settings.skip_tutorial = $Popup/ColorRect/SettingsContainer/SkipTutorial.pressed
+    settings.skip_tutorial = settings_container.get_node("SkipTutorial").pressed
