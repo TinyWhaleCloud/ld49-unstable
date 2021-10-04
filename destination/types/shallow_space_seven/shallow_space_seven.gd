d2beef4dcc728a1f760909a48ca3a4403172fa45
dysphoricUnicorn <git@dysphoric.dev>
2021-10-04 18:21:31 +0200

    Fix shallow space seven economic state

--- destination/types/shallow_space_seven/shallow_space_seven.gd
+++ destination/types/shallow_space_seven/shallow_space_seven.gd
@@ -2,7 +2,7 @@ extends "res://destination/destination.gd"
 
 
 func _ready():
-        stats = destination_stats.new('Shallow Space 7', 600, 0,0, 'Good', 'Expansion', 'A badly drawn, terribly "hidden" reference. But hey, people seem pretty friendly around here.', 1)
+        stats = destination_stats.new('Shallow Space 7', 600, 0,0, 'Good', 'Recovery', 'A badly drawn, terribly "hidden" reference. But hey, people seem pretty friendly around here.', 1)
 
 
 func _on_PassengerSpawner_passenger_spawned(start, end):
